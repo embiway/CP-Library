62914e86167421adc6aaef48964eaf3a2c7201b9
Lyndon Ho <44751422+Dingledooper@users.noreply.github.com>
2019-11-23 20:24:11 -0800

    Create StaticCHT.cpp

---
+++ data-structures/StaticCHT.cpp
@@ -0,0 +1,48 @@
+/*
+    - init()
+        Time:  O(1)
+        Space: O(1)
+    - insert()
+        Time:  O(1)
+        Space: O(1)
+    - get()
+        Time:  O(1)
+        Space: O(1)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T>
+struct Line
+{
+    T m, b;
+    int id;
+    
+    T get(T x) { return m * x + b; }
+};
+
+template <const int MAXN, typename T>
+struct StaticCHT
+{
+    int ptr, sz;
+    Line<T> v[MAXN];
+    
+    void init() { sz = ptr = 0; }
+    
+    bool bad(Line<T>& a, Line<T>& b, Line<T>& c) { return (a.b - c.b) * (b.m - a.m) <= (c.m - a.m) * (a.b - b.b); }
+    
+    void insert(Line<T> x)
+    {
+        while (ptr + 1 < sz && bad(v[sz - 2], v[sz - 1], x)) --sz;
+        v[sz++] = x;
+    }
+    
+    pair<T, int> get(T x)
+    {
+        ptr = min(ptr, sz);
+        while (ptr + 1 < sz && v[ptr].get(x) <= v[ptr + 1].get(x)) ++ptr;
+        return { v[ptr].get(x), v[ptr].id };
+    }
+};
