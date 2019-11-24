542bd4e7601f2b76ef944242bed1aed6e4d4b6c7
Lyndon Ho <44751422+Dingledooper@users.noreply.github.com>
2019-11-23 21:30:44 -0800

    Create BIT.cpp

---
+++ data-structures/BIT.cpp
@@ -0,0 +1,61 @@
+/*
+    - CONSTRUCTION
+        Time:  O(1)
+        Space: O(N)
+        
+    - void init()
+        Time:  O(1)
+        Space: O(1)
+        
+    - void init(auto& a, int sz)
+        Time:  O(N)
+        Space: O(1)
+        
+    - void update(int i, T x)
+        Time:  O(log N)
+        Space: O(1)
+        
+    - T query(int i)
+        Time:  O(log N)
+        Space: O(1)
+        
+    - T query(int l, int r)
+        Time:  O(log N)
+        Space: O(1)
+*/
+
+template <const int MAXN, typename T>
+struct BIT
+{
+    T bit[MAXN];
+    
+    void init() { memset(bit, 0, sizeof(bit)); }
+    
+    void init(auto& a, int sz)
+    {
+        memset(bit, 0, sizeof(bit));
+        for (int i = 1; i <= sz; i++)
+        {
+            bit[i] += a[i];
+            int idx = i + (i & -i);
+            if (idx <= sz) bit[idx] += bit[i];
+        }
+    }
+    
+    void update(int i, T x)
+    {
+        for (; i < MAXN; i += i & -i) bit[i] += x;
+    }
+    
+    T query(int i)
+    {
+        T res = 0;
+        for (; i > 0; i -= i & -i) res += bit[i];
+        return res;
+    }
+    
+    T query(int l, int r)
+    {
+        return query(r) - query(l - 1);
+    }
+};
