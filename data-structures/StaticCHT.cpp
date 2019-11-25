2b8bb03cf7b07731d12764eb6a3afed66b4e5abb
Lyndon Ho <44751422+Dingledooper@users.noreply.github.com>
2019-11-24 18:26:13 -0800

    Update StaticCHT.cpp

--- data-structures/StaticCHT.cpp
+++ data-structures/StaticCHT.cpp
@@ -29,7 +29,7 @@ struct Line
     T m, b;
     int id;
     
-    T get(T x) { return m * x + b; }
+    T get(const T x) { return m * x + b; }
 };
 
 template <const int MAXN, typename T>
@@ -40,15 +40,15 @@ struct StaticCHT
     
     void init() { sz = ptr = 0; }
     
-    bool bad(Line<T>& a, Line<T>& b, Line<T>& c) { return (a.b - c.b) * (b.m - a.m) <= (c.m - a.m) * (a.b - b.b); }
+    bool bad(const Line<T>& a, const Line<T>& b, const Line<T>& c) { return (a.b - c.b) * (b.m - a.m) <= (c.m - a.m) * (a.b - b.b); }
     
-    void insert(Line<T> x)
+    void insert(const Line<T> x)
     {
         while (ptr + 1 < sz && bad(v[sz - 2], v[sz - 1], x)) --sz;
         v[sz++] = x;
     }
     
-    pair<T, int> get(T x)
+    pair<T, int> get(const T x)
     {
         ptr = min(ptr, sz);
         while (ptr + 1 < sz && v[ptr].get(x) <= v[ptr + 1].get(x)) ++ptr;
