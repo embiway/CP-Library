0cee83ed7901efe1243bc2f31d05c4bab4e3d244
Dingledooper <ho3.14159@gmail.com>
2019-11-27 18:35:43 -0800

    Create BFS.hpp

---
+++ src/graph/BFS.hpp
@@ -0,0 +1,81 @@
+/*
+	Implementation of BFS (Breadth-First Search) with std::queue.
+	min_path() returns the dist[] array, where the disconnected nodes
+	have a value of INT_MAX. The get_path() function takes in an argument,
+	the destination, and returns a vector of a shortest path from the
+	source (given in min_path() argument), to the destination.
+
+	- CONSTRUCTION
+		Time:  O(1)
+		Space: O(V)
+
+	- void add(int u, int v)
+		Time:  O(1)
+		Space: O(1)
+
+	- void add_bi(int u, int v)
+		Time:  O(1)
+		Space: O(1)
+
+	- int* min_path(int u)
+		Time:  O(V + E)
+		Space: O(V + E)
+
+	- vector<int> get_path(int v)
+		Time:  O(V)
+		Space: O(V)
+*/
+
+#pragma once
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <const int V>
+struct BFS
+{
+	vector<int> adj[V + 1];
+	queue<int> q;
+	int dist[V + 1], parent[V + 1];
+
+	void add(int u, int v) { adj[u].push_back(v); }
+
+	void add_bi(int u, int v) { add(u, v); add(v, u); }
+
+	int* min_path(int u)
+	{
+		queue<int>().swap(q);
+		fill(dist, dist + V + 1, INT_MAX);
+		memset(parent, -1, sizeof(parent));
+
+		q.push(u);
+		dist[u] = 0;
+
+		while (!q.empty())
+		{
+			int curr = q.front();
+			q.pop();
+
+			for (int i : adj[curr])
+			{
+				if (dist[i] == INT_MAX)
+				{
+					parent[i] = curr;
+					dist[i] = dist[curr] + 1;
+					q.push(i);
+				}
+			}
+		}
+		return dist;
+	}
+
+	vector<int> get_path(int v)
+	{
+		vector<int> res;
+		for (; v != -1; v = parent[v]) res.push_back(v);
+		reverse(res.begin(), res.end());
+		return res;
+	}
+
+	void clear() { for (int i = 0; i <= V; i++) adj[i].clear(); }
+};
