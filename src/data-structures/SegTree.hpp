90f4755d89810011c930a0b96d3e7fd794258f86
Dingledooper <ho3.14159@gmail.com>
2019-12-08 19:57:15 -0800

    Fix SegTree

--- src/data-structures/SegTree.hpp
+++ src/data-structures/SegTree.hpp
@@ -2,7 +2,7 @@
 	Implementation of iterative Segment Tree which supports range-min
 	query and point update. The Segment Tree is useful because it is
 	very versatile, as it can do basically any math function as a
-	query. N is the size of the array.
+	query. Keep in mind that the queries are 0-indexed, and range is [l, r).
 
 	- CONSTRUCTION
 		Time:  O(1)
@@ -37,20 +37,18 @@ struct SegTree
 		for (int i = sz - 1; i; i--) tree[i] = max(tree[i << 1], tree[i << 1 | 1]);
 	}
 
-	void update(int i, T v)
+	void update(int i, const T v, const int sz)
 	{
-		i += N;
-		tree[i] = v;
-		for (; i > 1; i >>= 1) tree[i >> 1] = max(tree[i], tree[i ^ 1]);
+		for (i += sz, tree[i] = v; i; i >>= 1) tree[i >> 1] = max(tree[i], tree[i ^ 1]);
 	}
 
-	int query(int l, int r)
+	int query(int l, int r, const int sz)
 	{
 		T res = 0;
-		for (l += N, r += N; l < r; l >>= 1, r >>= 1)
+		for (l += sz, r += sz; l < r; l >>= 1, r >>= 1)
 		{
 			if (l & 1) res = max(res, tree[l++]);
-			if (r & 1) res = max(res, tree[r--]);
+			if (r & 1) res = max(res, tree[--r]);
 		}
 		return res;
 	}
