cff4ef45e92e043c5e2d0ebafb8dbdde26c6a9a7
Dingledooper <ho3.14159@gmail.com>
2019-12-11 19:54:05 -0800

    Add binary search

---
+++ src/misc/Search.hpp
@@ -0,0 +1,53 @@
+/*
+	A collection of different searching methods, using binary search.
+	The lower_bound() and upper_bound() methods are implemented using
+	binary search. TODO: Interpolation Search, Jump Search, Ternary Search.
+*/
+
+#pragma once
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace Search
+{
+	template <typename T, typename F>
+	T binary_search(T lo, T hi, F f)
+	{
+		T mid;
+		while (lo < hi)
+		{
+			mid = lo + (hi - lo) / 2;
+			auto temp = f(mid);
+			if (temp < 0) lo = mid + 1;
+			else if (temp > 0) hi = mid - 1;
+			else return mid;
+		}
+		return -1;
+	}
+
+	template <typename T, typename F>
+	T lower_bound(T lo, T hi, F f)
+	{
+		T mid;
+		while (lo < hi)
+		{
+			mid = lo + (hi - lo) / 2;
+			if (f(mid)) hi = mid;
+			else        lo = mid + 1;
+		}
+		return lo;
+	}
+
+	template <typename T, typename F>
+	T upper_bound(T lo, T hi, F f)
+	{
+		T mid;
+		while (lo < hi)
+		{
+			mid = lo + (hi - lo) / 2;
+			if (f(mid)) lo = mid + 1;
+			else        hi = mid;
+		}
+	}
+}
