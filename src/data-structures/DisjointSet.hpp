f627dc0c2cbb34f4eb9074950ac6fd09c79bd255
Dingledooper <ho3.14159@gmail.com>
2020-02-05 20:12:14 -0800

    Fix DisjointSet.hpp

--- src/data-structures/DisjointSet.hpp
+++ src/data-structures/DisjointSet.hpp
@@ -6,46 +6,44 @@ using namespace std;
 /*
     Supports Disjoint Set find() and union(), and also some other functions.
     With path compression, time complexity is O(α(N)).
-    
+
     - CONSTRUCTION
         Time:  O(1)
         Space: O(N)
-    
+
     - void init(const int N = MAXN)
         Time:  O(N)
         Space: O(1)
-        
+
     - int find(const int v)
         Time:  O(α(N))
         Space: O(α(N))
-        
+
     - bool join(int v, int w)
         Time:  O(α(N))
         Space: O(α(N))
-        
+
     - bool connected(const int v, const int w)
         Time:  O(α(N))
         Space: O(α(N))
-        
+
     - bool size(const int v)
         Time:  O(1)
         Space: O(1)
 */
 
 template <const int MAXN>
-struct DisjointSet
-{
+struct DisjointSet {
     int parent[MAXN + 1], sz[MAXN + 1];
-    
+
     void init(const int N = MAXN) {
-        for (int i = 1; i <= N; i++) parent[i] = i;
-        memset(sz, 1, sizeof(sz));
+        for (int i = 1; i <= N; i++) parent[i] = i, sz[i] = 1;
     }
-    
+
     int find(const int v) {
     	return v == parent[v] ? v : parent[v] = find(parent[v]);
     }
-    
+
     bool join(int v, int w) {
         if ((v = find(v)) == (w = find(w))) return false;
         if (sz[v] < sz[w]) swap(v, w);
@@ -53,12 +51,12 @@ struct DisjointSet
         sz[v] += sz[w];
         return true;
     }
-    
+
     bool connected(const int v, const int w) {
     	return find(v) == find(w);
     }
-    
-    bool size(const int v) {
+
+    int size(const int v) {
     	return sz[find(v)];
     }
 };
