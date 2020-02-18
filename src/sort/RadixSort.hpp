2c35cd202ec426e364c8c9d62fbad78099c3051d
Dingledooper <ho3.14159@gmail.com>
2020-02-17 16:32:17 -0800

    do stuff no. 1

--- src/sort/RadixSort.hpp
+++ src/sort/RadixSort.hpp
@@ -14,7 +14,7 @@ void radix_sort_unsigned(It st, It en) {
 	int bits = sizeof(T) << 3, range = en - st, *cnt = new int[256];
 	T *bucket = new T[range];
 	for (int p = 0; p < bits; p += 8) {
-	    memset(cnt, 0, 1024);
+		memset(cnt, 0, 1024);
 		for (It i = st; i != en; i++) ++cnt[(*i >> p) & 255];
 		for (int i = 1; i < 256; i++) cnt[i] += cnt[i - 1];
 		for (It i = en - 1; i != st - 1; i--) bucket[--cnt[(*i >> p) & 255]] = *i;
