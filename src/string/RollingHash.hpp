f3c86f8426fcfdfe3483678034140d7bd6dba0a0
Dingledooper <ho3.14159@gmail.com>
2020-02-22 15:46:29 -0800

    Fix RollingHash.hpp

--- src/string/RollingHash.hpp
+++ src/string/RollingHash.hpp
@@ -1,5 +1,5 @@
 /*
-	Rolling Hash for fast computation of substring hashes
+	Rolling Hash for fast computation of substring hashes (0-indexed [l, r])
 	Time complexity: O(N) init, O(1) get_hash, concat
 	 where N is the size of the string
 */
@@ -9,16 +9,15 @@
 
 template <const int MAXN, typename T, const T MOD, const int OFFSET, const int INDEXING>
 struct RollingHash {
-	template <typename T> T mod(T a, T m) { T res = a % m; return res < 0 ? res + m : res; }
-	template <typename T> T add_mod(T a, T b, T m) { return mod(a + b, m); }
-	T sub_mod(T a, T b, T m) { return mod(a - b, m); }
-	T mul_mod(T a, T b, T m) { T res = 0; for (; b; a = add_mod(a, a, m), b >>= 1) if (b & 1) res = add_mod(res, a, m); return res; }
-	T pre[MAXN + 1], hsh[MAXN + 1];
-	void init(It st, It en, const T BASE = MOD / 2 + 5) {
-		pre[0] = 1, hsh[N] = 0;
+	T pre[MAXN + 1], hsh[MAXN + 1]; const int WORD_SIZE = sizeof(T);
+	T add_mod(T a, T b) { T res = a + b; if (res >= MOD) res -= MOD; return res; }
+	T sub_mod(T a, T b) { T res = a - b; if (res < 0) res += MOD; return res; }
+	T mul_mod(T a, T b) { return WORD_SIZE <= 4 ? uint64_t(a) * b % MOD : __int128_t(a) * b % MOD; }
+	template <typename It> void init(It st, It en, const T BASE = MOD / 2 + 5) {
+		int N = en - st; pre[0] = 1, hsh[N] = 0;
 		for (int i = 1; i <= N; i++) pre[i] = mul_mod(pre[i - 1], BASE);
-		for (It i = st; i != en; i++) hsh[i] = add_mod(mul_mod(hsh[i + 1], BASE), *i - OFFSET + 1);
+		for (int i = N - 1; i >= 0; i--) hsh[i] = add_mod(mul_mod(hsh[i + 1], BASE), *(st + i) - OFFSET + 1);
 	}
-	T get_hash(int l, int r) { l -= INDEXING, r -= INDEXING; return sub_mod(hsh[l], mul_mod(hsh[r], pre[r - l])); }
+	T get_hash(int l, int r) { l -= INDEXING, r -= INDEXING - 1; return sub_mod(hsh[l], mul_mod(hsh[r], pre[r - l])); }
 	T concat(T a, T b, int alen) { return add_mod(a, mul_mod(b, pre[alen])); }
 };
