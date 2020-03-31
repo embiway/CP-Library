257bbd1059aeae5c6ea28b38575dadcb8ac0c53b
Dingledooper <ho3.14159@gmail.com>
2020-03-31 11:09:02 -0700

    Fix TarjanLCA

--- src/graph/TarjanLCA.hpp
+++ src/graph/TarjanLCA.hpp
@@ -7,20 +7,18 @@
 
 #pragma once
 #include <bits/stdc++.h>
-
-template <const int MAXV, typename T, const int INDEXING>
+template <const int MAXV, const int MAXQ, typename T, const int INDEXING>
 struct TarjanLCA {
 	std::vector<std::pair<int, T>> adj[MAXV]; std::vector<std::pair<int, int>> query[MAXV];
-	std::pair<int, int> ans[MAXV]; T dist[MAXV]; bool vis[MAXV]; int anc[MAXV], par[MAXV], sz[MAXV], idx = 0;
+	std::pair<int, T> ans[MAXQ]; T dist[MAXV]; bool vis[MAXV]; int anc[MAXV], par[MAXV], sz[MAXV], idx = 0;
 	void add(int u, int v, T w) { u -= INDEXING, v -= INDEXING; adj[u].emplace_back(v, w); adj[v].emplace_back(u, w); }
 	void add_query(int u, int v) { u -= INDEXING, v -= INDEXING; query[u].emplace_back(v, idx); query[v].emplace_back(u, idx++); }
-	void dfs1(int cur, int prev, T d) { dist[cur] = d; for (auto &i : adj[cur]) if (i.first != prev) dfs1(i.first, cur, d + i.second); }
-	void dfs2(int cur, int prev) {
-		anc[cur] = cur, vis[cur] = true;
-		for (auto &i : adj[cur]) if (i.first != prev) { dfs2(i.first, cur); join(cur, i.first); anc[find(cur)] = cur; }
+	void dfs(int cur, int prev, T d) {
+		dist[cur] = d, anc[cur] = cur, vis[cur] = true;
+		for (auto &i : adj[cur]) if (i.first != prev) { dfs(i.first, cur, d + i.second); join(cur, i.first); anc[find(cur)] = cur; }
 		for (auto &i : query[cur]) if (vis[i.first]) { int a = anc[find(i.first)]; ans[i.second] = {a, dist[cur] + dist[i.first] - 2 * dist[a]}; }
 	}
-	void process(int root, int V = MAXV) { std::fill(vis, vis + V, false); for (int i = 0; i < V; i++) par[i] = i, sz[i] = 1; dfs1(root -= INDEXING, -1, 0); dfs2(root, -1); }
+	void process(int root, int V = MAXV) { std::fill(vis, vis + V, false); for (int i = 0; i < V; i++) par[i] = i, sz[i] = 1; dfs(root -= INDEXING, -1, 0); }
 private:
 	int find(int v) { return v == par[v] ? v : par[v] = find(par[v]); }
 	bool join(int u, int v) {
